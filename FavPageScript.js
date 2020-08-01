1a9cdf43c0c35c05928a68eda774911b9b3f79a0
Maor Berkover <mahosh1983@gmail.com>
2020-08-01 22:43:42 +0200

    Removable favorite gallery items

--- FavPageScript.js
+++ FavPageScript.js
@@ -3,7 +3,6 @@ let favList = ["Tel Aviv"]
 function reboot() {
     createGallery(favKeys, favList)
 }
-
 function createGallery(keys, cities) {
     for(i=0 ; i<keys.length ; i++){
         let y = [];
@@ -19,10 +18,28 @@ function createGallery(keys, cities) {
                 icon = y[0].WeatherIcon;
                 temp = y[0].Temperature.Imperial.Value;
                 let a = document.createElement("DIV");  
-                a.setAttribute("class", "gallrey-item");
                 a.innerHTML = "<p class='cityName'>" + cities[index] + "</p>";
+                if(icon < 4 || icon == 5) {
+                    a.innerHTML += "<p class='type'>Sunny</p>";
+                    a.setAttribute("class", "gallrey-item sunny");
+                } else if (icon == 4 || icon == 6 || icon == 20 || icon == 21){
+                    a.innerHTML += "<p class='type'>Partially cloudy</p>";
+                    a.setAttribute("class", "gallrey-item partiallyCloudy");
+                } else if((icon > 6 && icon < 12) || icon == 19) {
+                    a.innerHTML += "<p class='type'>Cloudy</p>";
+                    a.setAttribute("class", "gallrey-item cloudy");
+                } else if(icon > 11 && icon < 19) {
+                    a.innerHTML += "<p class='type'>Rainy</p>";
+                    a.setAttribute("class", "gallrey-item rainy");
+                } else {
+                    a.innerHTML += "<p class='type'>Snowy</p>";
+                    a.setAttribute("class", "gallrey-item snowy");
+                }
                 a.innerHTML += "<p class='temp'>" + temp + " °F</p>";
+                a.innerHTML += "<div class='favIcon' onclick='this.parentNode.parentNode.removeChild(this.parentNode); return false;'><img src='Assests/Icons/favoriteFullIcon.png'></div>";
                 g.appendChild(a);
             });
     }
 }
+
+
