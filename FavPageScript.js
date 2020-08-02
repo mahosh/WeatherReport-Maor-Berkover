43eb8dcebd048c555691ef59277d94bc942bdb11
Maor Berkover <mahosh1983@gmail.com>
2020-08-02 10:42:44 +0200

    Removing fav city now also remove the city from the lists

--- FavPageScript.js
+++ FavPageScript.js
@@ -1,10 +1,47 @@
 let favKeys = [215854];
-let favList = ["Tel Aviv"]
+let favList = ["Tel Aviv"];
+let numOfFavs = 0;
 function reboot() {
-    createGallery(favKeys, favList)
+    createGallery(favKeys, favList);
+    document.getElementById('favoritesContainer').addEventListener('click', checkFavItems);
 }
+function checkFavItems() {
+    let x = document.getElementsByClassName('gallrey-item');
+        let currentCount = 0;
+        for(i=0 ; i<x.length ; i++){
+            currentCount++;
+        }
+        if(!(currentCount == numOfFavs)) {
+            let foundMatch = false;
+            for(i=0 ; i<favList.length ; i++){
+                for(j=0 ; j<x.length ; j++){
+                    if(x[j].getAttribute('data') == favList[i]){
+                        foundMatch = true;
+                    }
+                }
+                if(!foundMatch){
+                    favList.splice(i, 1);
+                    favKeys.splice(i, 1);
+                    numOfFavs--;
+                    if(numOfFavs == 0){
+                        document.getElementById("emptyMessage").classList.remove("hide");
+                        document.getElementById('favoritesContainer').removeEventListener('click', checkFavItems);
+                    }
+
+                } else {
+                    foundMatch = false;
+                }
+            }
+            
+        }
+}
+
 function createGallery(keys, cities) {
+    if(keys.length > 0){
+        document.getElementById("emptyMessage").classList.add("hide");
+    }
     for(i=0 ; i<keys.length ; i++){
+        numOfFavs++;
         let y = [];
         let icon;
         let temp;
@@ -19,6 +56,7 @@ function createGallery(keys, cities) {
                 temp = y[0].Temperature.Imperial.Value;
                 let a = document.createElement("DIV");  
                 a.innerHTML = "<p class='cityName'>" + cities[index] + "</p>";
+                a.setAttribute("data", cities[index])
                 if(icon < 4 || icon == 5) {
                     a.innerHTML += "<p class='type'>Sunny</p>";
                     a.setAttribute("class", "gallrey-item sunny");
@@ -35,7 +73,7 @@ function createGallery(keys, cities) {
                     a.innerHTML += "<p class='type'>Snowy</p>";
                     a.setAttribute("class", "gallrey-item snowy");
                 }
-                a.innerHTML += "<p class='temp'>" + temp + " °F</p>";
+                a.innerHTML += "<div class='tempContainer'><p class='temp'>" + temp + "</p><spanclass='showFehrenheit active'> °F</span><span class='showCelcius'> °C</span></div>";
                 a.innerHTML += "<div class='favIcon' onclick='this.parentNode.parentNode.removeChild(this.parentNode); return false;'><img src='Assests/Icons/favoriteFullIcon.png'></div>";
                 g.appendChild(a);
             });
